2d9ebedd57e4a1f6dabea669dcf33f37b806360f
Dev Dixit <devdixitsocial@gmail.com>
2026-02-07 22:59:04 +0530

    chat ai backend setup init

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,12 @@
+import { Express } from "express";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import path from "path";
+
+export const setupSwagger = (app: Express) => {
+  const swaggerDocument = YAML.load(
+    path.join(__dirname, "../../swagger/swagger.yaml")
+  );
+
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+};

31c330b692f0b64255f9186920cdf678f48cac00
Dev Dixit <devdixitsocial@gmail.com>
2026-02-14 21:46:25 +0530

    Refactor PDF text extraction and chunking logic; switch to Langchain PDFLoader and RecursiveCharacterTextSplitter. Update TypeScript configuration to use 'nodenext' module resolution. error solved, just need to do some improvement

--- src/services/chatWithPdf.service.ts
+++ src/services/chatWithPdf.service.ts
@@ -1,58 +1,24 @@
-const pdfParse = require('pdf-parse');
+import 'dotenv/config';
+import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
+import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
 
-export const extractTextFromPdf = async (pdfBuffer: Buffer) => {
-  try {
-    const data = await pdfParse(pdfBuffer);
-    return data.text;
+const getChunkedDocsFromPDF = async () => {
+  try{
+    const path = process.env.PDF_PATH || '';
+    
+    const loader = new PDFLoader(path);
+    const docs = await loader.load();
+
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 200
+    });
+
+    const chunkedDocs = await textSplitter.splitDocuments(docs);
+    return chunkedDocs;
   }
-  catch (error) {
-    console.error('Error in extracting text from pdf', error);
-    return null;
-  }
-}
-
-export const chunkText = (text: string, chunkSize = 1000, overlap = 200) => {
-  // text --> full data of text
-  // chunkSize --> max char per chunk
-  // overlap --> no of overlapping char between chunks
-  try {
-    const chunks = [];
-    // make empty array for chunks
-    let startIndex = 0;
-    // start indexing from 0
-
-    while (startIndex < text.length) {
-      // while start index is less then the text.length this loop runs
-      const endIndex = startIndex + chunkSize;
-      // ending index will be 0 + 1000 = 1000
-      let chunk = text.slice(startIndex, endIndex);
-      // one chunk = text from 0th char from 1000th of char
-
-      if (endIndex < text.length) {
-        const lastPeriod = chunk.lastIndexOf('.');
-        const lastNewline = chunk.lastIndexOf('\n');
-        const breakPoint = Math.max(lastPeriod, lastNewline);
-
-        if (breakPoint > chunkSize * 0.5) {
-          chunk = chunk.slice(0, breakPoint + 1);
-          startIndex += breakPoint + 1;
-        } else {
-          startIndex += chunkSize - overlap;
-        }
-      } else {
-        startIndex = text.length;
-      }
-
-      const trimmedChunk = chunk.trim();
-      if (trimmedChunk.length > 0) {
-        chunks.push(trimmedChunk);
-      }
-    }
-
-    return chunks;
-  }
-  catch (error) {
-    console.error('Error in chunking text', error);
+  catch(error) {
+    console.error('Error in chunking doc from pdf', error);
     return null;
   }
 }
